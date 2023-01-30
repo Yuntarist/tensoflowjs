36f31ec3b063d171c55d756fb698269a3fab91d0
Yuntarist <ftaruftk@naver.com>
2023-01-30 17:37:43 +0900

    230130

--- teamproject230127/teamproject230127/src/views/AboutView.vue
+++ teamproject230127/teamproject230127/src/views/AboutView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="dtd">
-    <h1>This is an about page</h1>
+  <input type="file" onchange="readExcel()" />
+  <br />
+  아버지키를 입력해주세요 <input type="number" id="key" />
+  <button onclick="bun()">분석시작</button>
+  <br />
+  <div style="width: 900px; height: 900px">
+    <!--그래프-->
+    <canvas id="myChart"></canvas>
   </div>
 </template>
 
-<!-- <script>
-  export default{
-    name:'app',
-    data(){
-      return{
-      }},
-      methods:{
-      }}
-    </script> -->
 <script setup>
 console.log('hello world')
 </script>
