0c0e8ae436a764d34c219b5ed4dcdc435eec1ac5
Yuntarist <ftaruftk@naver.com>
2023-02-03 17:49:22 +0900

    230203

--- vue_tfjs/v01/v01node.js
+++ vue_tfjs/v01/v01node.js
@@ -2,12 +2,22 @@ const express = require('express')
 const path = require('path')
 const logger = require('morgan')
 const app = express()
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const history = require('connect-history-api-fallback')
 const cw = require('./public/crawling.js')
 
 const _path = path.join(__dirname, '/dist')
 console.log(_path)
+app.use(history()) // 새로고침해도 안날라가는 모듈
 app.use('/', express.static(_path))
 app.use(logger('tiny'))
+
+app.use(
+  createProxyMiddleware('/v1', {
+    target: 'https://openapi.naver.com',
+    changeOrigin: true
+  })
+)
 
 app.get('/hak', (req, res) => {
   console.log('준비')
