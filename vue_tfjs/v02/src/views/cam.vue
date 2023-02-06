8174cb78d82fb0cfa376c70edca71c1f1a92c053
Yuntarist <ftaruftk@naver.com>
2023-02-06 17:37:01 +0900

    230206

--- vue_tfjs/v02/src/views/cam.vue
+++ vue_tfjs/v02/src/views/cam.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>dtd</div>
+  <div>Cam</div>
+  <button @click="webcamLaunch()">캠켜</button>
+  <div id="display"></div>
 </template>
 
 <script>
@@ -10,7 +12,22 @@ export default {
   data() {
     return {}
   },
-  methods: {}
+  methods: {
+    webcamLaunch: function () {
+      const display = document.getElementById('display') // 캠 화면이 켜지는 곳
+      const videoElement = document.createElement('video')
+      //웹캠 이미지를 표시하기 위한 요소릉 추가
+      display.appendChild(videoElement)
+      videoElement.width = 500
+      videoElement.height = 500
+      const webcamIterator = tf.data.webcam(videoElement)
+
+      //img: 입력 웹캠 이미지를 나타내는 텐서
+      const img = webcamIterator.capture()
+      img.print()
+      this.$tfjs.render.dtd(document.getElementById('display'), display)
+    }
+  }
 }
 </script>
 
