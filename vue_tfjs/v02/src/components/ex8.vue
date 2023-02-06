47efdb7a8a256468fef6bbfb1afea3600802fc70
Yuntarist <ftaruftk@naver.com>
2023-02-06 16:26:35 +0900

    230206

--- vue_tfjs/v02/src/components/ex8.vue
+++ vue_tfjs/v02/src/components/ex8.vue
@@ -8,46 +8,47 @@
 // eslint-disable-next-line
 /* eslint-disable */
 import Chart from 'chart.js/auto'
+import { data } from './data' // 강사님이 주신 js파일을 그냥 꼽아넣음
 function chart() {
   const ctx = document.getElementById('myChart')
-  const dataset = {
-    labels: [
-      'Eating',
-      'Drinking',
-      'Sleeping',
-      'Designing',
-      'Coding',
-      'Cycling',
-      'Running'
-    ],
-    datasets: [
-      {
-        label: 'My First Dataset',
-        data: [65, 59, 90, 81, 56, 55, 40],
-        fill: true,
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgb(255, 99, 132)',
-        pointBackgroundColor: 'rgb(255, 99, 132)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgb(255, 99, 132)'
-      },
-      {
-        label: 'My Second Dataset',
-        data: [38, 48, 40, 19, 96, 37, 100],
-        fill: true,
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        borderColor: 'rgb(54, 162, 235)',
-        pointBackgroundColor: 'rgb(54, 162, 235)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgb(54, 162, 235)'
-      }
-    ]
-  }
+  // const dataset = {
+  //   labels: [
+  //     'Eating',
+  //     'Drinking',
+  //     'Sleeping',
+  //     'Designing',
+  //     'Coding',
+  //     'Cycling',
+  //     'Running'
+  //   ],
+  //   datasets: [
+  //     {
+  //       label: 'My First Dataset',
+  //       data: [65, 59, 90, 81, 56, 55, 40],
+  //       fill: true,
+  //       backgroundColor: 'rgba(255, 99, 132, 0.2)',
+  //       borderColor: 'rgb(255, 99, 132)',
+  //       pointBackgroundColor: 'rgb(255, 99, 132)',
+  //       pointBorderColor: '#fff',
+  //       pointHoverBackgroundColor: '#fff',
+  //       pointHoverBorderColor: 'rgb(255, 99, 132)'
+  //     },
+  //     {
+  //       label: 'My Second Dataset',
+  //       data: [38, 48, 40, 19, 96, 37, 100],
+  //       fill: true,
+  //       backgroundColor: 'rgba(54, 162, 235, 0.2)',
+  //       borderColor: 'rgb(54, 162, 235)',
+  //       pointBackgroundColor: 'rgb(54, 162, 235)',
+  //       pointBorderColor: '#fff',
+  //       pointHoverBackgroundColor: '#fff',
+  //       pointHoverBorderColor: 'rgb(54, 162, 235)'
+  //     }
+  //   ]
+  // }
   new Chart(ctx, {
     type: 'radar',
-    data: dataset, // data : data 를 줄인것
+    data, // data : data 를 줄인것
     options: {
       responsive: false,
       elements: {
@@ -61,10 +62,6 @@ function chart() {
 </script>
 
 <style>
-#myChart {
-  width: 900px;
-  height: 400px;
-}
 #btnEx8 {
   display: flex;
   justify-content: center;
