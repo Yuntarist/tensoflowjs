f2b734cc022eb2c61022c5435483e18df53fde92
Yuntarist <ftaruftk@naver.com>
2023-02-07 14:23:25 +0900

    230207

--- vue_tfjs/v02/src/views/cam.vue
+++ vue_tfjs/v02/src/views/cam.vue
@@ -2,18 +2,19 @@
   <div>
     <div id="display"></div>
     <button @click="main()">스샷 분석</button>
+    <button @click="papago()">번역해</button>
     <button @click="alog()">디버거</button>
     <div>{{ result }}</div>
-    <div>{{ trans }}</div>
+    <div id="dtd">{{ trans }}</div>
   </div>
 </template>
 
 <script>
 // eslint-disable-next-line
 /* eslint-disable */
+import axios from 'axios' // 엑시오스 사용
 import * as tf from '@tensorflow/tfjs' // 텐서 플로우 모듈 불러오기
 import * as mobilenet from '@tensorflow-models/mobilenet' // 모바일 넷 모듈 불러오기
-
 const VideoElement = document.createElement('video') // 캠 태그 생성됨.
 export default {
   name: 'app',
@@ -48,6 +49,33 @@ export default {
         '(' +
         (cresult[0].probability * 100).toFixed(2) + // 수치가 나옴.
         '%)'
+    },
+    papago: function () {
+      const cid = process.env.VUE_APP_nid
+      const csecret = process.env.VUE_APP_npw
+      const query = this.result
+      const url = '/v1/papago/n2mt'
+      this.trans = '번역중...'
+      const frm = new FormData()
+      frm.append('source', 'en') // 네이버가 정해놓은것
+      frm.append('target', 'ko') // v01의 papago.js는 한글을 영어로 변환하는 거였으므로 그 두개를 바꿔줌.
+      frm.append('text', query)
+
+      axios
+        .post(url, frm, {
+          // axios api 로그인
+          headers: {
+            // headers: axios 안에 정해진 설정
+            'Content-Type': 'application/json',
+            'X-Naver-Client-Id': cid,
+            'X-Naver-Client-Secret': csecret
+          }
+        })
+        .then((res) => {
+          const rst = res.data.message.result.translatedText
+          console.log(rst)
+          this.trans = rst
+        })
     }
   }
 }
