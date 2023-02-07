252c196bfc9ec92e0bcb3efd0a56564ab79e0ad4
Yuntarist <ftaruftk@naver.com>
2023-02-07 16:19:22 +0900

    230207

--- vue_tfjs/v02/src/views/img.vue
+++ vue_tfjs/v02/src/views/img.vue
@@ -16,6 +16,8 @@
       <hr />
       <div id="result1">{{ result1 }}</div>
       <button @click="predict()">분석</button>
+      <button @click="papago()">번역</button>
+      <!-- 분석과 번역 함수를 한번에 넣었을 때 동시에 실행되므로 번역이 되질 않았음. -->
       <div id="result">{{ result }}</div>
       <div v-if="trans" id="trans">{{ trans }}</div>
     </div>
@@ -25,6 +27,7 @@
 <script>
 // eslint-disable-next-line
 /* eslint-disable */
+import axios from 'axios'
 import * as tf from '@tensorflow/tfjs'
 import * as mobilenet from '@tensorflow-models/mobilenet'
 export default {
@@ -67,6 +70,32 @@ export default {
           const tfimg = tf.browser.fromPixels(img)
           tfimg.print()
           tf.dispose(tfimg)
+        })
+    },
+    papago: function () {
+      const cid = process.env.VUE_APP_nid
+      const csecret = process.env.VUE_APP_npw
+      const query = this.result
+      const url = '/v1/papago/n2mt'
+      const frm = new FormData()
+      frm.append('source', 'en') // 네이버가 정해놓은것
+      frm.append('target', 'ko') // v01의 papago.js는 한글을 영어로 변환하는 거였으므로 그 두개를 바꿔줌.
+      frm.append('text', query)
+
+      axios
+        .post(url, frm, {
+          // axios api 로그인
+          headers: {
+            // headers: axios 안에 정해진 설정
+            'Content-Type': 'application/json',
+            'X-Naver-Client-Id': cid,
+            'X-Naver-Client-Secret': csecret
+          }
+        })
+        .then((res) => {
+          const rst = res.data.message.result.translatedText // 어지간하면 바꿀일 없을 듯?
+          console.log(rst)
+          this.trans = rst
         })
     }
   }
