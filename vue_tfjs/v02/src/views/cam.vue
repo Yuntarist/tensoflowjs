c841dc0a5a5f6d75d6bdd9378237c4f03290e5fa
Yuntarist <ftaruftk@naver.com>
2023-02-07 10:26:40 +0900

    230207

--- vue_tfjs/v02/src/views/cam.vue
+++ vue_tfjs/v02/src/views/cam.vue
@@ -21,19 +21,9 @@ export default {
       videoElement.width = 500
       videoElement.height = 500
       const webcamIterator = tf.data.webcam(videoElement)
-
-      //img: 입력 웹캠 이미지를 나타내는 텐서
-      // const img = webcamIterator.capture()
-      // img.print()
-      this.$tfjs.render.dtd(document.getElementById('display'), display)
     }
   }
 }
-
-// 나오긴 하는데 에러가 많이뜸
-// GET http://localhost:8080/data-load.ts net::ERR_ABORTED 404 (Not Found)
-// Refused to execute script from 'http://localhost:8080/data-load.ts' because its MIME type ('text/html') is not executable, and strict MIME type checking is enabled.
-// runtime-core.esm-bundler.js?d2dd:236 Uncaught TypeError: Cannot read properties of undefined (reading 'render')at Proxy.webcamLaunch
 </script>
 
 <style></style>
