400ffcefdb75eb8aa370cfb78984ec22a034a1f4
Yuntarist <ftaruftk@naver.com>
2023-03-08 14:33:38 +0900

    230308

---
+++ mevn3/ts/dist/start.js
@@ -0,0 +1,8 @@
+"use strict";
+const a = 3;
+const b = 'hello world';
+function abc(a, b) {
+    console.log(a);
+    console.log(b);
+}
+abc(a, b);
---
+++ mevn3/ts/src/start.ts
@@ -0,0 +1,7 @@
+const a: number = 3
+const b: string = 'hello world'
+function abc(a: number, b: string): any {
+  console.log(a)
+  console.log(b)
+}
+abc(a, b)
