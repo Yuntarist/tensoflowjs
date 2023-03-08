c311ac558ebde0bed61749f4dc46a5e528abb72e
Yuntarist <ftaruftk@naver.com>
2023-03-08 15:09:41 +0900

    230308

--- mevn3/vtf/src/views/HomeView.vue
+++ mevn3/vtf/src/views/HomeView.vue
@@ -1,5 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <h2>특강 Part3. tfjs 활용</h2>
+    <a href="http://www.tensorflow.org/js">www.tensorflow.org</a>
+    <hr />
+    <h3>외부링크들</h3>
+    <ol>
+      <li v-for="i of 9" :key="i">
+        <a :href="'http://localhost:8080/danfo0' + i + '.html'">{{
+          `danfo0${i}`
+        }}</a>
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script>
